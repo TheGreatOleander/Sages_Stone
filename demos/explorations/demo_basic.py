bf1f3c6e8eeaf01b063eb01b4a42ea3d1f8cff75
TheGreatOleander <thegreatoleander@gmail.com>
2026-02-02 17:36:11 -0500

    Delete demos directory

--- demos/explorations/demo_basic.py
+++
@@ -1,9 +0,0 @@
-from systems.core.lens_base import Lens
-
-class DoubleLens(Lens):
-    def apply(self, value):
-        return value * 2
-
-if __name__ == "__main__":
-    lens = DoubleLens("double")
-    print("Demo result:", lens(21))
